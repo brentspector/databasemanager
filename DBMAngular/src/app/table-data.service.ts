14e33d4a792eaaa753af272ad668946dc43cdfa7
brentspector <brent.spector@yahoo.com>
2018-07-14 17:20:52 -0700

    Created "Add" options for EditView

--- DBMAngular/src/app/table-data.service.ts
+++ DBMAngular/src/app/table-data.service.ts
@@ -65,6 +65,28 @@ export class TableDataService {
     this.showTable = table;
   }
 
+  addNewRecord(record: Object) {
+    return of(this.tableData[this.showTable].contents.push(record)).pipe(catchError(this.handleError('Add record', [])));
+  }
+
+  addMassNewRecords(files: FileList, uploadType: string) {
+    return of(this.appendMultipleRows(this.tableData[this.showTable].contents)).pipe(catchError(this.handleError('Add mass records', [])));
+  }
+
+  private appendMultipleRows(newSet: Object) {
+    const tempArray = this.tableData[this.showTable].contents;
+    for (const key in newSet) {
+      if (newSet.hasOwnProperty(key)) {
+        tempArray.push(newSet[key]);
+      }
+    }
+    this.tableData[this.showTable].contents = tempArray;
+  }
+
+  addNewColumn(columnObject: Object) {
+    return of(this.uploadStatus).pipe(catchError(this.handleError('Add new column', [])));
+  }
+
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
--- DBMAngular/src/app/table-edit/table-edit.component.ts
+++ DBMAngular/src/app/table-edit/table-edit.component.ts
@@ -13,15 +13,22 @@ export class TableEditComponent implements OnInit {
 
   modalGroup: FormGroup;
   modalFields: any[];
+  columnGroup: FormGroup;
 
   constructor(private modalService: NgbModal, private tableDataService: TableDataService) {}
+
+  ngOnInit() {
+    this.columnGroup = new FormGroup({
+      columnName: new FormControl(),
+      columnType: new FormControl('Choose...'),
+      columnTypevar1: new FormControl(''),
+      columnTypevar2: new FormControl('')
+   });
+  }
 
   open(content) {
     this.modalGroup = this.generateFormGroup();
     this.modalService.open(content).result.then((result) => {});
-  }
-
-  ngOnInit() {
   }
 
   generateFormGroup() {
@@ -37,7 +44,19 @@ export class TableEditComponent implements OnInit {
     return new FormGroup(group);
   }
 
+    private updateFormGroup(column: string, newValue: string) {
+      this.columnGroup.value[column] = newValue;
+  }
+
   private submitNewRecord() {
-    console.log(this.modalGroup.value);
+    this.tableDataService.addNewRecord(this.modalGroup.value);
+  }
+
+  private submitFile(files: FileList, uploadType: string) {
+    this.tableDataService.addMassNewRecords(files, uploadType);
+  }
+
+  private submitNewColumn() {
+    this.tableDataService.addNewColumn(this.columnGroup.value);
   }
 }
